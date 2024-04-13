2f2ca0b4517f3fa53e598c4e0f46c250bdebe4c9
Firdous <firdousrahaman42@gmail.com>
2024-04-14 00:12:27 +0530

    Add Image component to FeaturedCourses

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -3,6 +3,7 @@ import courseData from "../data/music_courses.json"
 import { BackgroundGradient } from "./ui/background-gradient";
 import Link from "next/link"
 import Button from "./Button";
+import Image from "next/image";
 
 type Course = {
     id: number;
@@ -33,7 +34,9 @@ export default function FeaturedCourses() {
                                 <BackgroundGradient
                                     className="flex flex-col rounded-[22px] bg-white dark:bg-gray-950 overflow-hidden h-full max-w-sm">
                                     <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow space-y-5">
-                                        <img src={course.image} alt={course.title} className="w-full h-40 object-cover rounded-lg" />
+                                        <Image src={course.image} alt={course.title}
+                                            width={400} height={200}
+                                            className="w-full h-40 object-cover rounded-lg" />
                                         <div>
                                             <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
                                             <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">{course.description}</p>
